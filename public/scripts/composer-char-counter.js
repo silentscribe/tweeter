9fe0190e356121800759c23f9dc9cbbebd2326f1
silentscribe <seelive.i@gmail.com>
2019-01-22 22:01:07 +0000

    tweetbox counter functioning and changing color when below 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  console.log('Document is Ready');
+
+const tweet = document.querySelector('#tweetbox');
+
+  function textCounter (event) {
+    let counter = document.querySelector('.counter');
+    let counterValue = 140 - this.value.length;
+    if (counterValue < 0) {
+      counter.style.color = 'red';
+      counter.innerText = counterValue;
+    } else {
+      counter.innerText = counterValue;
+      counter.style.color = 'black';
+    }
+  }
+
+  tweet.addEventListener('keyup', textCounter);
+
+});
